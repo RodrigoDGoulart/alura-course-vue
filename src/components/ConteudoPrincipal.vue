6b7acb960c94f4ce1178f326b9fa85f53149283b
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-03-30 18:45:33 -0300

    feat: get categories by API

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,11 +1,14 @@
 <script lang="ts">
+import SelecionarIngredientes from './SelecionarIngredientes.vue';
+
 export default {
   data() {
     return {
       ingredientes: ['Alho', 'Manteiga', 'Orégano']
       // ingredientes: []
     }
-  }
+  },
+  components: {SelecionarIngredientes}
 }
 </script>
 
@@ -27,6 +30,8 @@ export default {
         Sua lista está vazia, selecione ingredientes para iniciar.
       </p>
     </section>
+
+    <SelecionarIngredientes />
   </main>
 </template>
 
